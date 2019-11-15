ded7684fc7054c73f9fe55c889dba3dcf3209b95
Michal2002 <rozmiej@wp.pl>
2019-11-15 09:41:17 +0100

    fifisan

--- db/baza.sql
+++ db/baza.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS menu;
 CREATE TABLE menu (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	plik CHAR(20) NOT NULL,
@@ -9,11 +9,21 @@ CREATE TABLE menu (
 INSERT INTO menu VALUES(NULL, 'witam', 'Witamy', 1);
 INSERT INTO menu VALUES(NULL, 'formularz', 'Formularz', 2);
 INSERT INTO menu VALUES(NULL, 'klasa', 'Klasa', 3);
+INSERT INTO menu VALUES(NULL, 'userfrom', 'Użydkownik', 4);
 
-CREATE TABLE menu (
+CREATE TABLE posty (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-tresc VARCHAR NOT NULL,
-
+	tresc VARCHAR NOT NULL
 );
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	login CHAR(20) NOT NULL,
+	haslo CHAR(40),
+	email VARCHAR(50),
+	data DATE
+);
+
 -- INSERT INTO menu(tytul, plik, id) VALUES('Klasa', 'klasa', NULL);
 -- sqlite3 baza.db < baza.sql
--- inc/db.php
+++ inc/db.php
@@ -10,7 +10,8 @@ function init_tables() {
 	if (file_exists(DBASE.'baza.sql')) {
 		$sql = file_get_contents(DBASE.'baza.sql', 'r');
 		$q = "SELECT name FROM sqlite_master WHERE type='table' AND name='menu'";
-		$ret = $db->query($q);
+		$ret = array();
+		db_query($q, $ret);
 		if (empty($ret)) {
 			$db->exec($sql);
 			$kom[] = "Utworzono tabele!";
@@ -19,15 +20,17 @@ function init_tables() {
 }
 function db_query($q, &$ret) {
 	global $db;
+	$r = null;
 	try {
-		$r = $db->query($q, PDO::FETCH_ASSOC);
+		$r = $db->query($q);
 	} catch(PDOException $e) {
 		echo ($e->getMessage());
 	}
 	if ($r) {
-		$ret = $r->fetchAll();
+		$ret = $r->fetchAll(PDO::FETCH_ASSOC);
 		return true;
 	}
 	return false;
 }
 ?>
+
